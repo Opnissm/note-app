d41ab06def041cabfe00b90d10dc34fd4e74fd45
mark <markdgarnica@gmail.com>
2023-01-12 14:25:35 +0800

    Change index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,33 +1,12 @@
 import { CookiesProvider } from "react-cookie";
-import {
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-} from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { AuthProvider } from "./context/auth-context";
-import AuthenticatedPage from "./AuthenticatedPage";
 import "./index.css";
 import App from "./App";
-import TinyEditor from "./components/TinyEditor";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route element={<AuthProvider />}>
-      <Route path="/" element={<App />} />
-      <Route path="/note" element={<AuthenticatedPage />}>
-        <Route path=":noteId" element={<TinyEditor />} />
-      </Route>
-    </Route>
-  )
-);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <RouterProvider router={router}>
-    <CookiesProvider>
-      <App />
-    </CookiesProvider>
-  </RouterProvider>
+  <CookiesProvider>
+    <App />
+  </CookiesProvider>
 );
