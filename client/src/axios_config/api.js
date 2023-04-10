21d795dccdfeada1e5110309a4a0d431b0983152
markdanielgarnica <markdgarnica@gmail.com>
2023-04-11 00:52:49 +0800

    add prod url

--- client/src/axios_config/api.js
+++ client/src/axios_config/api.js
@@ -1,6 +1,14 @@
 import axios from "axios";
 
+let url;
+
+if (process.env.NODE_ENV === "development") {
+  url = "http://localhost:5000";
+} else if (process.env.NODE_ENV === "production") {
+  url = "https://note-app-x68g.onrender.com";
+}
+
 export default axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: url,
   withCredentials: true,
 });
