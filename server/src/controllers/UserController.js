a484bbca4b3766612a93c256df0437687806ede6
markdanielgarnica <markdgarnica@gmail.com>
2023-04-12 15:42:00 +0800

    add both secure and httponly

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -11,7 +11,7 @@ const {
   validatePasswordRequestToken,
 } = require("../utils/validatePasswordRequestToken");
 exports.logout = async (req, res, next) => {
-  res.clearCookie("token");
+  res.clearCookie("token", { secure: true, httpOnly: true });
   return res.json({ isSuccessful: true });
 };
 exports.login = async (req, res, next) => {
@@ -47,7 +47,7 @@ exports.login = async (req, res, next) => {
       username: usernameInDb,
       email: emailInDb,
     });
-    res.cookie("token", token);
+    res.cookie("token", token, { secure: true, httpOnly: true });
     return res.json({
       token,
       user: { username: usernameInDb, email: emailInDb, userId },
@@ -131,7 +131,7 @@ exports.signup = async (req, res, next) => {
       email: newUserEmail,
       username: newUserUsername,
     });
-    res.cookie("token", token);
+    res.cookie("token", token, { secure: true, httpOnly: true });
     return res.json({
       token,
       isSuccessful: true,
